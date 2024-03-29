3c9acfef692af9c83ba6433bb736345c6f993f39
Jesse Nay <jessenay26@gmail.com>
2024-03-28 20:36:20 -0600

    added stuff to login page

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -21,3 +21,15 @@ export const ADD_SKILL = gql`
     }
   }
 `;
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
