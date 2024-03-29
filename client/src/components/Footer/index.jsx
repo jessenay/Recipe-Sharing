293a02225ada16b647318bf32893dfe762a55643
maglb <magali.lb21@gmail.com>
2024-03-28 20:37:04 -0600

    footer added

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -4,17 +4,19 @@ const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
   return (
-    <footer className="w-100 mt-auto text-dark p-4">
+    <footer
+      className="w-100 mt-auto text-dark p-4 align-center"
+      style={{ backgroundColor: "#06052e" }}
+    >
       <div className="container text-center mb-5">
-        {location.pathname !== '/' && (
-          <button
-            className="btn btn-dark mb-3"
-            onClick={() => navigate(-1)}
-          >
+        {location.pathname !== "/" && (
+          <button className="btn btn-dark mb-3" onClick={() => navigate(-1)}>
             &larr; Go Back
           </button>
         )}
-        <h4>&copy; {new Date().getFullYear()} - Tech Friends</h4>
+        <h4 className="text-light">
+          &copy; {new Date().getFullYear()} - Recipe Sharing
+        </h4>
       </div>
     </footer>
   );
