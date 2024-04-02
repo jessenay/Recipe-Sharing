f544cd02e152334ad13555fef2c2d21a15d6350d
maglb <magali.lb21@gmail.com>
2024-04-01 22:47:13 -0600

    styling added

--- client/src/components/RecipeItem/index.jsx
+++ client/src/components/RecipeItem/index.jsx
@@ -1,8 +1,28 @@
 export default function RecipeItem(props) {
   return (
-    <div className="card" >
+    <div className="card">
       <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
+        <h3
+          style={{
+            fontSize: "20px",
+            fontFamily: "Poppins",
+            fontWeight: 600,
+            textTransform:'uppercase',
+          }}
+        >
+          {props.category}
+        </h3>
+        <h5
+          className="card-title"
+          style={{
+            fontSize: "38px",
+            fontFamily: "Playfair Display",
+            fontWeight: 400,
+            fontStyle: "italic",
+          }}
+        >
+          {props.title}
+        </h5>
         <p className="card-text">{props.instruction}</p>
         <a href="#" className="card-link">
           View Recipe
