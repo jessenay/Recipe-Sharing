e79ba2afff21dc12f5730df3b7ca7595148f9f7f
maglb <magali.lb21@gmail.com>
2024-04-01 21:55:44 -0600

    reciep list component imported

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import RecipeList from "./components/RecipeList";
+import RecipeList from "../components/RecipeList";
 
 const Home = () => {
   const navigate = useNavigate();
