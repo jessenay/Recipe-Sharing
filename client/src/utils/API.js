f116d1e55efe1a32496f53f7deeb2989ce1d9a56
maglb <magali.lb21@gmail.com>
2024-04-02 21:47:14 -0600

    type error

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,8 +4,8 @@ export const getMe = (token) => {
         'Content-Type': 'application/json',
         authorization: `Bearer ${token}`,
       },
-    });
-  };
+    };
+ 
   
   export const createUser = (userData) => {
     return fetch('/api/users', {
@@ -42,27 +42,6 @@ export default { search };
       authorization: `Bearer ${token}`,
     },
   });
-};
-
-export const createUser = (userData) => {
-  return fetch('/api/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  });
-};
-
-export const loginUser = (userData) => {
-  return fetch('/api/users/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  });
-};
 
 export const fetchAllRecipes = async () => {
   try {
