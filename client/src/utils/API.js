bf818f861159550f7096d19e4af527599200b062
maglb <magali.lb21@gmail.com>
2024-04-02 21:08:45 -0600

    fetch function via axios added

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -33,7 +33,7 @@ export const getMe = (token) => {
 import axios from "axios";
 
 const search = async () =>
-  axios.get(`www.themealdb.com/api/json/v1/1/random.php`);
+  axios.get(`https://www.themealdb.com/api/json/v1/1/random.php`);
 
 export default { search };
   
