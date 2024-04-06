e8e0ccab38a49dbdf56ee27ffdfac729ec0796cf
maglb <magali.lb21@gmail.com>
2024-04-05 22:47:16 -0600

    styling added

--- client/src/components/RecipeCard/index.jsx
+++ client/src/components/RecipeCard/index.jsx
@@ -7,18 +7,29 @@ const RecipeCard = ({ recipe }) => {
       <h2 className="word">{recipe.title}</h2>
       <img src={recipe.image} alt={recipe.title} />
 
-      { recipe.prepTime && <p className="time">Prep Time: {recipe.prepTime}</p>}
-      { recipe.cookTime && <p className="time">Cook Time: {recipe.cookTime}</p>}
+      {recipe.prepTime && <p className="time">Prep Time: {recipe.prepTime}</p>}
+      {recipe.cookTime && <p className="time">Cook Time: {recipe.cookTime}</p>}
       <h3 className="word">Ingredients</h3>
-      <ul className="list">
+      <ul
+        className="list"
+        style={{
+          textAlign: "center",
+          fontFamily: "Roboto",
+          fontSize: "18px",
+        }}
+      >
         {recipe.ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
+          <li style={{ marginBottom: "1%" }} key={index}>
+            {ingredient}
+          </li>
         ))}
       </ul>
       <h3 className="word">Instructions</h3>
-      <ul className="list">
+      <ul className="list" style={{ fontFamily: "Roboto", fontSize: "18px" }}>
         {recipe.instructions.map((instruction, index) => (
-          <li key={index}>{instruction}</li>
+          <li style={{ marginBottom: "2%", lineHeight: '22px' }} key={index}>
+            {instruction}
+          </li>
         ))}
       </ul>
     </div>
