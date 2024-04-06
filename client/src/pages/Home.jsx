0b97ac21a95654dfef04398797323a4edbdc8622
maglb <magali.lb21@gmail.com>
2024-04-05 22:38:46 -0600

    styling updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,20 +2,16 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import RecipeList from "../components/RecipeList";
 import AuthService from "../utils/auth";
-import { NewRecipe } from '../components/RecipeCards/NewRecipe';
-import RecipeCard from "../components/RecipeCards/RecipeCard.jsx"
-import RandomRecipeGenerator from "../components/RandomRecipeGenerator";
-import auth from "../utils/auth";
+import { NewRecipe } from "../components/RecipeCards/NewRecipe";
 
 const Home = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!AuthService.loggedIn()) {
-      navigate('/');
+      navigate("/");
     }
   }, [navigate]);
-
 
   const handleAddRecipeClick = () => {
     navigate("/add-recipe");
@@ -80,7 +76,54 @@ const Home = () => {
         </div>
       </section>
 
-      <RandomRecipeGenerator />
+      <div
+        className="pt-4"
+        style={{
+          borderBottom: "solid black 2px",
+          marginTop: "2%",
+        }}
+      >
+        <div className="card-body">
+          <h3
+            style={{
+              fontSize: "20px",
+              fontFamily: "Poppins",
+              fontWeight: 600,
+              textTransform: "uppercase",
+              textAlign: "center",
+            }}
+          >
+            Recipe Generator
+          </h3>
+          <h2
+            className="card-title"
+            style={{
+              fontSize: "45px",
+              fontFamily: "Playfair Display",
+              fontWeight: 400,
+              textAlign: "center",
+            }}
+          >
+            Don't know what to cook?
+          </h2>
+          <Link className="text-light p-2" to="/random-recipe">
+            <button
+              style={{
+                fontSize: "22px",
+                fontFamily: "Poppins",
+                fontWeight: 500,
+                textTransform: "uppercase",
+                margin: "20px auto",
+                display: "block",
+              }}
+              href="#"
+              className="btn"
+            >
+              Generate a Recipe
+            </button>
+          </Link>
+        </div>
+      </div>
 
       <NewRecipe />
 
@@ -90,7 +133,6 @@ const Home = () => {
           <RecipeList />
         </div>
       </section>
-
     </main>
   );
 };
