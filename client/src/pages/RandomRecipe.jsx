d0b0461124457f08e6e8f80693f40e4d792b7870
maglb <magali.lb21@gmail.com>
2024-04-05 21:42:41 -0600

    random recipe display code added

--- client/src/pages/RandomRecipe.jsx
+++ client/src/pages/RandomRecipe.jsx
@@ -1,15 +1,46 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import RandomRecipeCard from "../components/RandomRecipeCard";
-
+import React, { useState, useEffect } from "react";
+import RecipeCard from "../components/RecipeCard";
+import { search } from "../utils/API";
 
 const RandomRecipe = () => {
+  // create state for holding returned google api data
+  const [randomRecipe, setRandomRecipe] = useState();
 
-  return (
-    // <main>
-      <RandomRecipeCard />
-    // </main>
-  );
+  useEffect(() => {
+    search()
+      .then((res) => {
+        console.log(res.data.meals[0]);
+        const {
+          strMeal: title,
+          strMealThumb: image,
+          strInstructions: instructions,
+          ...meal
+        } = res.data.meals[0];
+        const ingredients = [];
+        for (let i = 1; i <= 20; i++) {
+          const measure = meal["strMeasure" + i];
+          const ingredient = meal["strIngredient" + i];
+          if (!measure || !ingredient) {
+            break;
+          }
+          ingredients.push(measure + " " + ingredient);
+        }
+        setRandomRecipe({
+          title,
+          image,
+          ingredients,
+          instructions: instructions.split("\r\n").filter(Boolean),
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  if (randomRecipe) {
+    console.log(randomRecipe);
+    return <RecipeCard recipe={randomRecipe} />;
+  } else {
+    return <div>Loading ...</div>;
+  }
 };
 
 export default RandomRecipe;
